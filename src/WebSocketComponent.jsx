a21ef1e601c245a740d57bf0c162422200279dfd
turtle <nguyenthanhsang60309@outlook.com>
2024-05-03 11:06:43 +0700

    update chat

--- src/WebSocketComponent.jsx
+++ src/WebSocketComponent.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { clientStomp } from "./websocket";
+// import { Client } from "@stomp/stompjs";
 
 export default function WebSocketComponent() {
   let [input, setInput] = useState("");
+  let [connect, setConnect] = useState(false);
+  let [chats, setChats] = useState([{}]);
+
+  clientStomp.onWebSocketClose = () => {
+    console.log("disconnect");
+    setConnect(false);
+  };
 
   const turnOnConnect = () => {
     clientStomp.activate();
@@ -21,18 +29,45 @@ export default function WebSocketComponent() {
     }
   };
 
+  const checkConnected = () => {
+    console.log(
+      "Connected " + clientStomp.connected ? "establish" : "was destroy"
+    );
+  };
+
+  clientStomp.onConnect = (frame) => {
+    console.log("Connect");
+    setConnect(true);
+    clientStomp.subscribe("/topic/greetings", (mess) => {
+      let txt = JSON.parse(mess.body);
+      setChats((chats) => {
+        return [...chats, txt];
+      });
+      console.log(chats);
+    });
+  };
   return (
     <>
       <h1>This is websocket</h1>
-      <button onClick={turnOnConnect}>Connect</button>
-      <button onClick={turnOffConnect}>Disconnect</button>
+      <button disabled={connect} onClick={turnOnConnect}>
+        Connect
+      </button>
+      <button disabled={!connect} onClick={turnOffConnect}>
+        Disconnect
+      </button>
+      <button onClick={checkConnected}>Check Connected</button>
       <form style={{ marginTop: "20px" }}>
         <label htmlFor="name"> Name </label>
         <input type="text" onInput={(e) => setInput(e.target.value)} />
-        <button type="button" onClick={sendName}>
+        <button disabled={!connect} type="button" onClick={sendName}>
           Send Name
         </button>
       </form>
+      <div className="">
+        {chats.map((value) => (
+          <p> {value.content}</p>
+        ))}
+      </div>
     </>
   );
 }
--- src/websocket.js
+++ src/websocket.js
@@ -2,19 +2,11 @@ import { Client } from "@stomp/stompjs";
 
 export const clientStomp = new Client({
     brokerURL: "ws://localhost:8080/spring-stomp",
-    onConnect: (frame) => {
-        console.log(frame);
-        clientStomp.subscribe("/topic/greetings", (messenger) => {
-            console.log(messenger.body);
-        })
-    },
     onStompError: (err) => {
         console.log(err);
     },
     onWebSocketError: (err) => {
         console.log(err);
     },
-    onWebSocketClose: (evt) => {
-        console.log(evt);
-    }
 })
+
